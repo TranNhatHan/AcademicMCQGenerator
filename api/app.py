600609dca76c7e7047b5dc55ad1a8e7b0e43bf4e
han <han.tran@my.ntu.ac.uk>
2025-11-23 23:03:23 +0000

    connect to the database

--- api/app.py
+++ api/app.py
@@ -1,6 +1,11 @@
 from bottle import Bottle, run, template
+import os
+import weaviate
 
 app = Bottle()
+
+WEAVIATE_URL = os.environ["WEAVIATE_URL"]
+client = weaviate.connect_to_local("weaviate", 8080)
 
 @app.get('/')
 def home():
@@ -9,5 +14,13 @@ def home():
 @app.route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
+
+@app.get('/weaviate/health')
+def weaviate_health():
+    return {
+        "ready": client.is_ready(),
+        "live": client.is_live(),
+        "weaviate_url": WEAVIATE_URL
+    }
 
 run(app, host="0.0.0.0", port=8000)
