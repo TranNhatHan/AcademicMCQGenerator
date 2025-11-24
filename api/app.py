f21253e631be58ec5e6e6249174a89a602b0c8da
Han Tran <han2k3x2@gmail.com>
2025-11-24 17:16:06 +0000

    resolve confict

--- api/app.py
+++ api/app.py
@@ -7,13 +7,16 @@ app = Bottle()
 WEAVIATE_URL = os.environ["WEAVIATE_URL"]
 client = weaviate.connect_to_local("weaviate", 8080)
 
+
 @app.get('/')
 def home():
     return {"message": "API is running"}
 
+
 @app.route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
+
 
 @app.get('/weaviate/health')
 def weaviate_health():
@@ -22,5 +25,6 @@ def weaviate_health():
         "live": client.is_live(),
         "weaviate_url": WEAVIATE_URL
     }
+
 
 run(app, host="0.0.0.0", port=8000)
